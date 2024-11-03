d5a4ea4e42d0bc0b6a09a84f94cc36717d19732e
Daniil Ponomarenko <den3free@gmail.com>
2024-11-03 10:30:38 +0300

    убирает return из стрелочных функций

--- js/main.js
+++ js/main.js
@@ -38,30 +38,24 @@ const generatePhotoId = createIdGenerator();
 const generateCommentId = createIdGenerator();
 const generateUrlId = createIdGenerator();
 
-const createComment = () => {
-  const comment = {
-    id: generateCommentId(),
-    avatar: `img/avatar-${getRandomNumber(MIN_ID_AVATAR, MAX_ID_AVATAR)}.svg`,
-    message: MESSAGES[getRandomNumber(0, MESSAGES.length - 1)],
-    name: NAMES[getRandomNumber(0, NAMES.length - 1)]
-  };
-  return comment;
-};
-
-const createDescriptionPhoto = () => {
-  const descriptionPhoto = {
-    id: generatePhotoId(),
-    url: `photos/${generateUrlId()}.jpg`,
-    description: 'Это очень новое фото, пока не добавили описание ))',
-    likes: getRandomNumber(MIN_LIKES, MAX_LIKES),
-    comments: Array.from({ length: getRandomNumber(MIN_COMMENTS, MAX_COMMENTS) }, createComment),
-  };
-  return descriptionPhoto;
-};
-
-const descriptionPhotos = Array.from({ length: SIMILAR_PHOTO_COUNT }, createDescriptionPhoto);
-
-// защита от Кекса (линтера)
-descriptionPhotos.forEach((el) => {
-  JSON.stringify(el);
+const createComment = () => ({
+  id: generateCommentId(),
+  avatar: `img/avatar-${getRandomNumber(MIN_ID_AVATAR, MAX_ID_AVATAR)}.svg`,
+  message: MESSAGES[getRandomNumber(0, MESSAGES.length - 1)],
+  name: NAMES[getRandomNumber(0, NAMES.length - 1)]
 });
+
+const createComments = () => Array.from({ length: getRandomNumber(MIN_COMMENTS, MAX_COMMENTS) }, createComment);
+
+const createDescriptionPhoto = () => ({
+  id: generatePhotoId(),
+  url: `photos/${generateUrlId()}.jpg`,
+  description: 'Это очень новое фото, пока не добавили описание ))',
+  likes: getRandomNumber(MIN_LIKES, MAX_LIKES),
+  comments: createComments()
+});
+
+const createDescriptionPhotos = () => Array.from({ length: SIMILAR_PHOTO_COUNT }, createDescriptionPhoto);
+
+createDescriptionPhotos();
+
