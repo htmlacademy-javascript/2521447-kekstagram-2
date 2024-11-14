46df90d7dbda376faccf2b124b5f6b2d54dcbb3c
Daniil Ponomarenko <den3free@gmail.com>
2024-11-14 12:20:10 +0300

    создает модуль отрисовки фотографий

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 import { createDescriptionPhotos } from './create-description-photos';
+import { renderDescriptionPhotos } from './render-description-photos';
 
-createDescriptionPhotos();
+const pictures = document.querySelector('.pictures');
+const photos = createDescriptionPhotos();
+const fragment = document.createDocumentFragment();
 
+photos.forEach((photo) => {
+  fragment.append(renderDescriptionPhotos(photo));
+});
+
+pictures.append(fragment);
