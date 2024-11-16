54b9065fe1e4797f1927f98ef303350c74284bba
Keks <keks@htmlacademy.ru>
2024-11-16 10:53:37 +0300

    :heavy_check_mark: Сборка #7

--- 7/js/main.js
+++ 7/js/main.js
@@ -1,12 +1,1 @@
-import { createDescriptionPhotos } from './create-description-photos';
-import { renderDescriptionPhotos } from './render-description-photos';
-
-const pictures = document.querySelector('.pictures');
-const photos = createDescriptionPhotos();
-const fragment = document.createDocumentFragment();
-
-photos.forEach((photo) => {
-  fragment.append(renderDescriptionPhotos(photo));
-});
-
-pictures.append(fragment);
+import './render-thumbnails';
---
+++ 7/js/render-thumbnails.js
@@ -0,0 +1,38 @@
+import { createThumbnails } from './create-thumbnails';
+
+const pictureTemplate = document.querySelector('#picture')
+  .content
+  .querySelector('.picture');
+
+const createTemplatePhoto = ({
+  url,
+  description,
+  likes,
+  comments,
+}) => {
+  const photoTemplate = pictureTemplate.cloneNode(true);
+  const photoImg = photoTemplate.querySelector('.picture__img');
+
+  photoImg.src = url;
+  photoImg.alt = description;
+  photoTemplate.querySelector('.picture__likes')
+    .textContent = likes;
+  photoTemplate.querySelector('.picture__comments')
+    .textContent = comments.length;
+
+  return photoTemplate;
+};
+
+const renderThumbnails = (photos) => {
+  const pictures = document.querySelector('.pictures');
+  const fragment = document.createDocumentFragment();
+
+  photos.map((photo) => {
+    fragment.append(createTemplatePhoto(photo));
+  });
+
+  return pictures.append(fragment);
+};
+
+renderThumbnails(createThumbnails());
+
